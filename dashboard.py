86d99268effe8efc482d0156a22e12f6821cd0ae
Robert_Mac <Robert_Mac@dhcp-128-189-253-188.ubcsecure.wireless.ubc.ca>
2019-11-28 15:29:51 -0800

    Update after working together

--- dashboard.py
+++ dashboard.py
@@ -5,86 +5,144 @@ from dash.dependencies import Input, Output
 from datetime import datetime as dt
 import pandas as pd
 import altair as alt
+import dash_bootstrap_components as dbc
 alt.data_transformers.disable_max_rows()
 
 df = pd.read_csv('https://raw.githubusercontent.com/UBC-MDS/Group_105/master/data/birdstrikes_clean.csv')
 
-app = dash.Dash(__name__, assets_folder='assets')
+app = dash.Dash(__name__, assets_folder='assets', external_stylesheets=[dbc.themes.CERULEAN])
 server = app.server
 
 app.title = 'Dash app with pure Altair HTML'
 
-app.layout = html.Div([
-    #dbc.Row(
-        #[
-    html.Label('Choose all Damage Levels you wish to show in plots below:'),
-    dcc.Checklist(
-        id = 'damage_types_dropdown',
-        options = [
-            {'label': 'No Damage', 'value': 'None'},
-            {'label': 'Minor Damage', 'value': 'Minor'},
-            {'label': 'Medium Damage', 'value': 'Medium'},
-            {'label': 'Substantial Damage', 'value': 'Substantial'}
-        ],
-        value = ['Minor', 'Medium', 'Substantial'], style=dict(width='71.5%')
-    ), #width=6, style={'border':'1px solid'}
+title_block = dbc.Jumbotron(
+    [
+        dbc.Container(
+            [
+                html.Img(src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Unico_Anello.png/1920px-Unico_Anello.png', 
+                      width='100px')
+                #html.H1("Cars! Cars! Explore Cars!", className="display-3"),
+                #html.P(
+                #    "Add a description of the dashboard",
+                #    className="lead",
+            ],
+            fluid=True,
+        )
+    ],
+    fluid=True,
+)
 
-    html.Iframe(
-        sandbox = 'allow-scripts',
-        id = 'line_plot',
-        height = '500',
-        width = '850',
-        style = {'border-width': '0'}
-    ),
+content = dbc.Container([
+    dbc.Row([
+                dbc.Col(
 
-    html.Label('Years:'),
-    dcc.RangeSlider(
-        id = 'date_slider',
-        marks = {i: '{}'.format(i) for i in range(1990, 2002)},
-        count = 12,
-        min = 1990,
-        max = 2002,
-        step = 1,
-        value = [1990, 2002]
-    ),
+                    #SELECTORS
 
-    html.Iframe(
-        sandbox = 'allow-scripts',
-        id = 'bar_plot',
-        height = '500',
-        width = '850',
-        style = {'border-width': '0'},
-        ),
+                    html.Label('Choose all Damage Levels you wish to show in plots below:'),
+                    dcc.Checklist(
+                        id = 'damage_types_dropdown',
+                        options = [
+                            {'label': 'No Damage', 'value': 'None'},
+                            {'label': 'Minor Damage', 'value': 'Minor'},
+                            {'label': 'Medium Damage', 'value': 'Medium'},
+                            {'label': 'Substantial Damage', 'value': 'Substantial'}
+                        ],
+                        value = ['Minor', 'Medium', 'Substantial']
+                    ),
 
-    html.Label('Choose X Variable for the Bar Chart:'),
-    dcc.RadioItems(
-        id = 'bar_radio',
-        options = [
-            {'label': 'Flight Phase', 'value': 'flight_phase'},
-            {'label': 'Time of Day', 'value': 'time_of_day'},
-            {'label': 'Bird Size', 'value': 'bird_size'}
-        ],
-        value = 'flight_phase'
-    ),
 
-    html.Label('Choose X Variable for the Heat Map'),
-    dcc.RadioItems(
-        id = 'heatmap_radio',
-        options = [
-            {'label': 'State', 'value': 'state'},
-            {'label': 'Airport', 'value': 'airport'},
-        ],
-        value = 'state'
-    ),
+                    html.Label('Years:'),
+                    dcc.RangeSlider(
+                        id = 'date_slider',
+                        marks = {i: '{}'.format(i) for i in range(1990, 2002)},
+                        count = 12,
+                        min = 1990,
+                        max = 2002,
+                        step = 1,
+                        value = [1990, 2002]
+                    ),
 
-    html.Iframe(
-        sandbox = 'allow-scripts',
-        id = 'heatmap_plot',
-        height = '950',
-        width = '870',
-        style = {'border-width': '0'}
-    )   
-] )#, style={'columnCount': 3}) # 
+
+                    html.Label('Choose X Variable for the Bar Chart:'),
+                    dcc.RadioItems(
+                        id = 'bar_radio',
+                        options = [
+                            {'label': 'Flight Phase', 'value': 'flight_phase'},
+                            {'label': 'Time of Day', 'value': 'time_of_day'},
+                            {'label': 'Bird Size', 'value': 'bird_size'}
+                        ],
+                        value = 'flight_phase'
+                    ),
+
+
+                    html.Label('Choose X Variable for the Heat Map'),
+                    dcc.RadioItems(
+                        id = 'heatmap_radio',
+                        options = [
+                            {'label': 'State', 'value': 'state'},
+                            {'label': 'Airport', 'value': 'airport'},
+                        ],
+                        value = 'state'
+                    )
+                    ]),
+
+                dbc.Col([
+
+                    dbc.Row([
+
+                        dbc.Col(
+
+                            #ADD GRAPH 1 Line Plot
+
+                            html.Iframe(
+                                sandbox = 'allow-scripts',
+                                id = 'line_plot',
+                                height = '500',
+                                width = '850',
+                                style = {'border-width': '0'}
+                            )
+                        , width=5 ),
+
+                        dbc.Col(
+
+                        #ADD GRAPH 2 Bar Plot
+
+                            html.Iframe(
+                                sandbox = 'allow-scripts',
+                                id = 'bar_plot',
+                                height = '500',
+                                width = '850',
+                                style = {'border-width': '0'},
+                            )
+
+                        , width=5), 
+                    ]),
+
+                    dbc.Row(
+
+                        dbc.Container([
+
+                        #ADD GRAPH 3 Heat Map
+
+                         html.Iframe(
+                            sandbox = 'allow-scripts',
+                            id = 'heatmap_plot',
+                            height = '950',
+                            width = '870',
+                            style = {'border-width': '0'}
+                        )
+
+                        ])
+
+
+                    )
+                ], width=2)
+            ])
+    ])
+
+app.layout = html.Div([title_block,
+                       content])
+
 
 @app.callback(
     dash.dependencies.Output(component_id = 'line_plot', component_property = 'srcDoc'),
